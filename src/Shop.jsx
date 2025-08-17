c88af47f92e9ab845ba448945e02527d23b7746c
nguyen_dat <manhdatvbhp@gmail.com>
2025-08-18 03:15:50 +0700

    first commit

---
+++ src/Shop.jsx
@@ -0,0 +1,12 @@
+import './Shop.css'
+import Navigation from './components/Navigation/Navigation.jsx';
+
+function Shop() {
+  return (
+    <div className="Shop">
+      <Navigation />
+    </div>
+  )
+}
+
+export default Shop;
