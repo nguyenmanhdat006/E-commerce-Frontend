aa69a2e7d993dad2a99d933d24df7a3ee889a550
nguyen_dat <manhdatvbhp@gmail.com>
2025-09-14 21:34:49 +0700

    [ADD] PRODUCT_DETAIL

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter  } from "react-router-dom";    
 import Shop from "./Shop";
 import ProductListPage from "./pages/ProductListPage/ProductListPage.jsx";
+import ProductDetails from "./pages/ProductDetailPage/ProductDetails.jsx";
 import ShopApplicationWrapper from "./pages/ShopApplicationWrapper.jsx";
  
 
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: "/men",
                 element: <ProductListPage categoryType="MEN" />
+            },
+            {
+                path: "/product/:productId",
+                element: <ProductDetails />
             }
         ]
     },
