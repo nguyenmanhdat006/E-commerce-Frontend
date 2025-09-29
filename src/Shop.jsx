bb23314624cba93237b5fc36e78e7a4fdade2b50
nguyen_dat <manhdatvbhp@gmail.com>
2025-09-29 23:28:32 +0700

    [ADD] setting redux/ redux toolkit

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -4,8 +4,30 @@ import NewArrivals from './components/Sections/NewArrivals.jsx';
 import Category from './components/Sections/Categories/Category.jsx';
 import content from './data/content.json';
 import Footer from './components/Footer/Footer.jsx';
+import { useEffect } from 'react';
+import { fetchCategories } from './api/fetchCategories.js';
+import { useDispatch } from 'react-redux';
+import { loadCategories } from './store/features/category.js';
+import { setLoading } from './store/features/common.js';
 
 const Shop = () => {
+
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(setLoading(true))
+    fetchCategories().then(res => {
+      dispatch(loadCategories(res))
+    
+        // eslint-disable-next-line no-unused-vars
+    }).catch(err => {
+
+    }).finally(()=> {
+      dispatch(setLoading(false))
+    })
+  }, [dispatch])
+
   return (
     <>
       <HeroSection />
